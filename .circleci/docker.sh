7ef92670b060b5d4d44ca11153b31196b2cae5a4
Andy Damevin <adamevin@redhat.com>
2019-07-22 17:27:21 +0200

    feat: add SD cicd scripts (#51)

--- .circleci/docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REVISION=$(echo $(git show --format=%H -s HEAD) | cut -c1-7)
-
-docker build -f ./src/main/docker/Dockerfile.jvm -t fabric8/launcher-quarkus:latest .
-docker tag fabric8/launcher-quarkus:latest fabric8/launcher-quarkus:$REVISION
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push fabric8/launcher-quarkus:latest
-docker push fabric8/launcher-quarkus:$REVISION
---
+++ build_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -exv
+
+IMAGE="quay.io/launcher/launcher-quarkus"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+
+mkdir -p tmp
+
+docker build -f src/main/docker/Dockerfile.native.multistage -t "${IMAGE}:${IMAGE_TAG}" .
+
+if [[ -n "$QUAY_USER" && -n "$QUAY_TOKEN" ]]; then
+    DOCKER_CONF="$PWD/.docker"
+    mkdir -p "$DOCKER_CONF"
+    docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+    docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
