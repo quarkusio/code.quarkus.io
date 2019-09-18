2ac23974ad195bd0e87e9d4baea497ae3d1bd587
ia3andy <adamevin@redhat.com>
2019-09-18 13:36:16 +0200

    Use build date UTC -24 hours for zip files

--- src/main/kotlin/io/quarkus/code/writer/CommonsZipProjectWriter.java
+++ src/main/kotlin/io/quarkus/code/writer/CommonsZipProjectWriter.java
@@ -23,12 +23,15 @@ import java.util.Set;
  * ProjectWriter implementation to create a zip.
  */
 public class CommonsZipProjectWriter implements ProjectWriter {
+
+    // Remove 1 day to be sure the date won't be in the past in any location
+    private static final Date CREATION = new Date(System.currentTimeMillis() - 24 * 3600000);
+
     private final ArchiveOutputStream aos;
     private final String basePath;
     private final Set<String> createdDirs = new HashSet<>();
     private final Map<String, ZipArchiveEntry> archiveEntriesByPath = new LinkedHashMap<>();
     private final Map<String, byte[]> contentByPath = new LinkedHashMap<>();
-    private final Date creation = new Date();
 
     private CommonsZipProjectWriter(final ArchiveOutputStream aos, final String basePath) {
         this.aos = aos;
@@ -41,7 +44,7 @@ public class CommonsZipProjectWriter implements ProjectWriter {
     }
 
     @Override
-    public String mkdirs(String path){
+    public String mkdirs(String path) {
         if (path.length() == 0) {
             return "";
         }
@@ -69,7 +72,6 @@ public class CommonsZipProjectWriter implements ProjectWriter {
             createdDirs.add(dirPathText);
         }
     }
-
 
 
     public void write(String path, byte[] contentBytes, boolean allowExec) {
@@ -109,9 +111,9 @@ public class CommonsZipProjectWriter implements ProjectWriter {
 
     private X5455_ExtendedTimestamp getTimestamp() {
         X5455_ExtendedTimestamp timestamp = new X5455_ExtendedTimestamp();
-        timestamp.setCreateJavaTime(creation);
-        timestamp.setModifyJavaTime(creation);
-        timestamp.setAccessJavaTime(creation);
+        timestamp.setCreateJavaTime(CREATION);
+        timestamp.setModifyJavaTime(CREATION);
+        timestamp.setAccessJavaTime(CREATION);
         return timestamp;
     }
 
