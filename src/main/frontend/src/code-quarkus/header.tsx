029fb5bfc4815405b2c40410e5a83a30b7698965
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-11-05 14:19:12 +0100

    fix: changed menu

--- src/main/frontend/src/code-quarkus/header.tsx
+++ src/main/frontend/src/code-quarkus/header.tsx
@@ -4,17 +4,21 @@ import './header.scss';
 export function Header() {
   return (
     <div className="header">
-      <div className="header-content responsive-container">
+      <div className="header-content">
         <div className="brand">
-          <a href="http://www.quarkus.io"><div className="brand-image" /></a>
+          <a href="/">
+            <img src="quarkus_logo_white.png" className="project-logo" title="Quarkus" />
+          </a>
         </div>
-        <ul className="nav">
-          <li><a href="https://quarkus.io/get-started/">Get Started</a></li>
-          <li><a href="https://quarkus.io/guides/">Guides</a></li>
-          <li><a href="https://quarkus.io/community/">Community</a></li>
-          <li><a href="https://quarkus.io/blog/">Blog</a></li>
-          <li><a className="active" href="https://code.quarkus.io/">Start Coding</a></li>
-        </ul>
+        <div className="nav-container">
+          <ul className="nav-list">
+            <li><a href="https://quarkus.io/get-started/">Get Started</a></li>
+            <li><a href="https://quarkus.io/guides/">Guides</a></li>
+            <li><a href="https://quarkus.io/community/">Community</a></li>
+            <li><a href="https://quarkus.io/blog/">Blog</a></li>
+            <li><a className="active" href="/">Start Coding</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
