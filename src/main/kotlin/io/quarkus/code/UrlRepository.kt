e9bfc327603476c740fab8bedcd16595b144db74
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-11-18 16:21:33 +0100

    removed common code and merged into one

--- src/main/kotlin/io/quarkus/code/UrlRepository.kt
+++ src/main/kotlin/io/quarkus/code/UrlRepository.kt
@@ -3,13 +3,17 @@ package io.quarkus.code
 import io.quarkus.code.model.ShortUrl
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest
+import java.util.HashMap
 import java.util.stream.Collectors
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 
 
 @ApplicationScoped
-open class UrlRepository: AbstractRepository() {
+open class UrlRepository {
     @Inject
     lateinit var dynamoDbClient: DynamoDbClient
 
@@ -27,7 +31,7 @@ open class UrlRepository: AbstractRepository() {
     }
 
     open fun save(shortUrl: ShortUrl): List<ShortUrl> {
-        dynamoDbClient.putItem(putRequest(shortUrl));
+        dynamoDbClient.putItem(putRequest(shortUrl))
         return findAll()
     }
 
@@ -43,4 +47,53 @@ open class UrlRepository: AbstractRepository() {
         }
         error("invalid")
     }
+
+    private fun scanRequest(): ScanRequest {
+        return ScanRequest.builder().tableName(tableName)
+                .attributesToGet(ID_COL, URL_COL).build()
+    }
+
+    private fun putRequest(shortUrl: ShortUrl): PutItemRequest {
+        val item = HashMap<String, AttributeValue>()
+        item[ID_COL] = AttributeValue.builder().s(shortUrl.id).build()
+        item[URL_COL] = AttributeValue.builder().s(shortUrl.url).build()
+
+        return PutItemRequest.builder()
+                .tableName(tableName)
+                .item(item)
+                .build()
+    }
+
+    private fun queryRequest(url: String): ScanRequest {
+        val values = HashMap<String, AttributeValue>()
+        values[":url"] = AttributeValue.builder().s(url).build()
+        val attributesNames = HashMap<String, String>()
+        attributesNames["#$URL_COL"] = URL_COL
+
+        return ScanRequest.builder()
+                .tableName(tableName)
+                .filterExpression("#$URL_COL = :url")
+                .expressionAttributeNames(attributesNames)
+                .expressionAttributeValues(values)
+                .build()
+    }
+
+    private fun getRequest(name: String): GetItemRequest {
+        val key = HashMap<String, AttributeValue>()
+        key[ID_COL] = AttributeValue.builder().s(name).build()
+
+        return GetItemRequest.builder()
+                .tableName(tableName)
+                .key(key)
+                .attributesToGet(ID_COL, URL_COL)
+                .build()
+    }
+
+    companion object {
+        const val ID_COL = "shortUrlId"
+        const val URL_COL = "shortUrlUrl"
+    }
+
+    private val tableName: String
+        get() = "shortUrls"
 }
