21c7de4afe30cbf065955c4746ece15125f6562e
Andy Damevin <ia3andy@gmail.com>
2019-11-22 15:48:25 +0100

    Revert "Enable https and use Provider for gaTrackingId"
    This reverts commit 8b7d81f2

--- src/main/kotlin/io/quarkus/code/analytics/GoogleAnalyticsService.kt
+++ src/main/kotlin/io/quarkus/code/analytics/GoogleAnalyticsService.kt
@@ -4,11 +4,12 @@ import com.brsanthu.googleanalytics.GoogleAnalytics
 import com.brsanthu.googleanalytics.GoogleAnalyticsConfig
 import com.brsanthu.googleanalytics.request.DefaultRequest
 import io.quarkus.code.services.CodeQuarkusConfigManager
+import javax.inject.Singleton
 import io.quarkus.runtime.StartupEvent
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import java.util.logging.Logger
 import javax.enterprise.event.Observes
 import javax.inject.Inject
-import javax.inject.Singleton
 
 
 @Singleton
@@ -24,7 +25,7 @@ open class GoogleAnalyticsService {
     var googleAnalytics: GoogleAnalytics? = null
 
     fun onStart(@Observes e: StartupEvent) {
-        if (googleAnalytics == null && config.gaTrackingId.get().isNotEmpty()) {
+        if (googleAnalytics == null && config.gaTrackingId.isNotEmpty()) {
             val defaultRequest = DefaultRequest()
 
             defaultRequest.documentHostName("code.quarkus.io")
@@ -32,17 +33,17 @@ open class GoogleAnalyticsService {
 
             googleAnalytics = GoogleAnalytics.builder()
                     .withDefaultRequest(defaultRequest)
-                    .withTrackingId(config.gaTrackingId.get())
+                    .withTrackingId(config.gaTrackingId)
                     .withConfig(GoogleAnalyticsConfig().setBatchSize(30).setBatchingEnabled(true))
                     .build()
-            log.info("GoogleAnalytics is enabled, trackingId: ${config.gaTrackingId.get()}")
+            log.info("GoogleAnalytics is enabled, trackingId: ${config.gaTrackingId}")
         } else {
             log.info("GoogleAnalytics is disabled")
         }
     }
 
     fun sendEvent(clientName: String? = "unknown", action: String, label: String) {
-        if (googleAnalytics != null) {
+        if(googleAnalytics != null) {
             googleAnalytics!!.event()
                     .campaignSource(clientName)
                     .eventCategory("API")
