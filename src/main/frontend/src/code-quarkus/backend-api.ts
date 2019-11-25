86fff29b1987efdbf38dd397487de6f96c5b63fc
Andy Damevin <ia3andy@gmail.com>
2019-11-25 12:15:55 +0100

    Google Analytics in the backend
    This reverts commit da8220a0

--- src/main/frontend/src/code-quarkus/backend-api.ts
+++ src/main/frontend/src/code-quarkus/backend-api.ts
@@ -1,8 +1,10 @@
 const backendUrl = process.env.REACT_APP_BACKEND_URL || "";
+export const CLIENT_NAME = 'Code Quarkus Frontend';
+const headers = { headers: { 'Client-Name': CLIENT_NAME}};
 
 export async function fetchExtensions() {
   try {
-    const data = await fetch(`${backendUrl}/api/extensions`);
+    const data = await fetch(`${backendUrl}/api/extensions`, headers);
     return await data.json();
   } catch(e) {
     throw new Error("Failed to load Quarkus extension list");
@@ -11,7 +13,7 @@ export async function fetchExtensions() {
 
 export async function fetchConfig() {
   try {
-    const data = await fetch(`${backendUrl}/api/config`);
+    const data = await fetch(`${backendUrl}/api/config`, headers);
     return await data.json();
   } catch(e) {
     return {
