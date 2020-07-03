35ad57425da4838f68c063b94a2fa5df507a513a
Andy Damevin <adamevin@redhat.com>
2020-07-03 12:08:51 +0200

    Fix ACAO (#364)

--- src/main/kotlin/io/quarkus/code/rest/CORSFilter.kt
+++ src/main/kotlin/io/quarkus/code/rest/CORSFilter.kt
@@ -23,17 +23,18 @@ class CORSFilter : ContainerRequestFilter {
     var response: HttpServletResponse? = null
 
     override fun filter(context: ContainerRequestContext) {
-        val requestedMethods = request?.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD.toString())
+        val origin = request?.getHeader(HttpHeaders.ORIGIN.toString()) ?: return
 
+        response?.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN.toString(), origin)
+
+        val requestedMethods = request?.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD.toString())
         if (requestedMethods != null) {
             response?.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS.toString(), requestedMethods)
         }
-        val requestedHeaders = request?.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS.toString())
 
+        val requestedHeaders = request?.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS.toString())
         if (requestedHeaders != null) {
             response?.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS.toString(), requestedHeaders)
         }
-
-        response?.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN.toString(), "*")
     }
 }
