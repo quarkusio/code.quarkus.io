27fe0297b7436686f4c8aac110d54c8cf24aa843
Andy Damevin <ia3andy@gmail.com>
2020-07-03 10:42:51 +0200

    Fix CORS problem

---
+++ src/main/kotlin/io/quarkus/code/rest/CORSFilter.kt
@@ -0,0 +1,39 @@
+package io.quarkus.code.rest
+
+import io.vertx.core.http.HttpHeaders
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import javax.ws.rs.container.ContainerRequestContext
+import javax.ws.rs.container.ContainerRequestFilter
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.UriInfo
+import javax.ws.rs.ext.Provider
+
+
+@Provider
+class CORSFilter : ContainerRequestFilter {
+
+    @Context
+    internal var info: UriInfo? = null
+
+    @Context
+    var request: HttpServletRequest? = null
+
+    @Context
+    var response: HttpServletResponse? = null
+
+    override fun filter(context: ContainerRequestContext) {
+        val requestedMethods = request?.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD.toString())
+
+        if (requestedMethods != null) {
+            response?.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS.toString(), requestedMethods)
+        }
+        val requestedHeaders = request?.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS.toString())
+
+        if (requestedHeaders != null) {
+            response?.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS.toString(), requestedHeaders)
+        }
+
+        response?.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN.toString(), "*")
+    }
+}
