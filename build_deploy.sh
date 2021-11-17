3b8f831d601174e6ff54f149ccf950633db65d8f
Andy Damevin <ia3andy@gmail.com>
2021-11-17 16:52:57 +0100

    Don't run CI/CD for dependabot bumps

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,6 +2,10 @@
 
 set -exv
 
+if [[ $(git --no-pager  log --oneline -1) == *Bump* ]]; then
+  exit 1
+fi
+
 (cd frontend;./build_deploy.sh)
 (cd api;./build_deploy.sh)
 (cd acceptance-test;./build_deploy.sh)
