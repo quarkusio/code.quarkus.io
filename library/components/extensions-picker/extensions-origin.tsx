48fa7cf206905a4ac33cf1879d257bf8ec8540cc
Andy Damevin <ia3andy@gmail.com>
2021-12-08 16:08:16 +0100

    Use new Quark Platform Icon

--- library/components/extensions-picker/extensions-origin.tsx
+++ library/components/extensions-picker/extensions-origin.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { SVGProps } from 'react';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { CgShapeHexagon } from 'react-icons/cg';
+
+// Generated using svgr
+function PlatformIcon(props: SVGProps<SVGSVGElement>){
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      enableBackground="new 0 0 32 32"
+      xmlSpace="preserve"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        className="platform-icon_svg__st0"
+        d="m16 4.1 10.3 6V22L16 28 5.7 22V10L16 4.1M16 0 2.1 8v16L16 32l13.9-8V8L16 0z"
+      />
+      <path
+        className="platform-icon_svg__st0"
+        d="M23.8 11.5c-.5-.8-1.6-1.1-2.4-.7L16 14l-5.4-3.1c-.8-.5-1.9-.2-2.4.6s-.2 1.9.6 2.4l5.4 3.1v6.2c0 1 .8 1.8 1.7 1.8 1 0 1.8-.8 1.8-1.7v-6.2l5.4-3.1c.9-.6 1.2-1.6.7-2.5z"
+      />
+    </svg>
+  );
+}
 
 export function ExtensionsOrigin(props: { platform: boolean }) {
   const Overlay = (
@@ -15,7 +38,7 @@ export function ExtensionsOrigin(props: { platform: boolean }) {
       delay={{ show: 200, hide: 0 }}
     >
       <span className="extension-origin-platform">
-        <CgShapeHexagon />
+        <PlatformIcon />
       </span>
     </OverlayTrigger> : <React.Fragment/>
   )
