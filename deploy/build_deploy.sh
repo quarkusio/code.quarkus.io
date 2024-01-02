34877b3b90b741cad9db6b51ed8b0a9808f5a33d
Andy Damevin <ia3andy@gmail.com>
2024-01-02 11:22:37 +0100

    Deploy stuff

--- deploy/build_deploy.sh
+++ deploy/build_deploy.sh
@@ -5,6 +5,6 @@ set -exv
 if [[ $(git --no-pager  log --oneline -1) == *Bump* ]]; then
   exit 1
 fi
-cd ..;
-./deploy/build_deploy_community_app.sh
+
+./deploy/build_deploy_community_app.sh;
 # ./deploy./build_deploy_acceptance_test.sh
