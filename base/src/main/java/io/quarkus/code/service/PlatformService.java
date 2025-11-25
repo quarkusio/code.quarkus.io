edbbd52e0ac636a755712e5c69619f72338ba05c
George Gastaldi <gegastaldi@gmail.com>
2025-11-25 15:26:09 -0300

    Change log level from DEBUG to FINE

--- base/src/main/java/io/quarkus/code/service/PlatformService.java
+++ base/src/main/java/io/quarkus/code/service/PlatformService.java
@@ -217,7 +217,7 @@ public class PlatformService {
         }
         if (platformServiceCacheRef.get() != null
                 && platformServiceCacheRef.get().platformTimestamp().equals(platformTimestamp)) {
-            LOG.log(Level.DEBUG, "The platform cache is up to date with the registry");
+            LOG.log(Level.FINE, "The platform cache is up to date with the registry");
             return;
         }
         Collection<Platform> platforms = platformCatalog.getPlatforms();
