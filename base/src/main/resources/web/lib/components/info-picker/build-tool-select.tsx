5fd8dfe2ed49ac6a8486419e38e3893445534348
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2026-03-18 18:52:36 +0100

    feat: Override for restriction build tools for streams

--- base/src/main/resources/web/lib/components/info-picker/build-tool-select.tsx
+++ base/src/main/resources/web/lib/components/info-picker/build-tool-select.tsx
@@ -2,20 +2,34 @@ import * as React from 'react';
 import { useAnalyticsEditionField } from '../../core/analytics';
 import { InputProps } from '../../core/types';
 import { FaAngleDown } from 'react-icons/fa';
+import { BuildToolCompatibility } from '../api/model';
 
-export const BuildToolSelect = (props: InputProps<string>) => {
+interface BuildToolSelectProps extends InputProps<string> {
+  buildToolCompatibility?: BuildToolCompatibility;
+}
+
+const BUILD_TOOL_LABELS: { [key: string]: string } = {
+  'MAVEN': 'Maven',
+  'GRADLE': 'Gradle',
+  'GRADLE_KOTLIN_DSL': 'Gradle with Kotlin DSL'
+};
+
+export const BuildToolSelect = (props: BuildToolSelectProps) => {
   const onChangeWithDirty = useAnalyticsEditionField('buildTool', props.onChange)[1];
   const adaptedOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     onChangeWithDirty(e.currentTarget.value);
   };
+
+  const availableTools = props.buildToolCompatibility?.tools || ['MAVEN', 'GRADLE', 'GRADLE_KOTLIN_DSL'];
+
   return (
     <div className="form-group form-group-select">
       <label className="form-group-label" htmlFor="buildtool" arial-label="Choose build tool"><span
         className="form-group-label-text">Build Tool</span></label>
       <select id="buildtool" value={props.value} aria-label="Edit build tool" onChange={adaptedOnChange} className={'form-group-control form-group-select'}>
-        <option value={'MAVEN'}>Maven</option>
-        <option value={'GRADLE'}>Gradle</option>
-        <option value={'GRADLE_KOTLIN_DSL'}>Gradle with Kotlin DSL</option>
+        {availableTools.map(tool => (
+          <option key={tool} value={tool}>{BUILD_TOOL_LABELS[tool]}</option>
+        ))}
       </select>
       <FaAngleDown />
     </div>
